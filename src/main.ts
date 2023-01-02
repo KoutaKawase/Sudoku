9f61443cb0b13e1b7ebb0db8cf1caf60b74155bf
KoutaKawase <suhrkawase@gmail.com>
2023-01-02 11:12:49 +0900

    feat: 🎸 wip

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,25 @@
 import 'virtual:windi.css';
 import './sudoku.css';
 
-// prettier-ignore
-const board = [
-  1, 1, 1, 1, 1, 1, 1, 1, 1,
-  1, 1, 1, 1, 1, 1, 1, 1, 1,
-  1, 1, 1, 1, 1, 1, 1, 1, 1,
-  1, 1, 1, 1, 1, 1, 1, 1, 1,
-  1, 1, 1, 1, 1, 1, 1, 1, 1,
-  1, 1, 1, 1, 1, 1, 1, 1, 1,
-  1, 1, 1, 1, 1, 1, 1, 1, 1,
-  1, 1, 1, 1, 1, 1, 1, 1, 1,
-  1, 1, 1, 1, 1, 1, 1, 1, 1,
-]
+type Numbers = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+type CellState = 'empty' | Numbers;
+
+class Cell {
+  private state: 'empty' | Numbers = 'empty';
+  private readonly element: HTMLElement;
+
+  constructor(state: CellState, elem: HTMLElement) {
+    this.state = state;
+    this.element = elem;
+  }
+
+  init() {
+    this.element.addEventListener('click', () => {
+      console.log(`Clicked Cell${this.element.id}`);
+    });
+  }
+}
+
 const boardElementCss =
   'border-2 border-neutral-500 grid grid-cols-3 grid-rows-3 w-4/5 mt-10';
 
@@ -46,5 +53,16 @@ if (boardContainerElement !== null) {
 
   boardContainerElement.appendChild(boardElement);
 }
+
+const board = [...Array(81)].map((_, i) => {
+  const cellElement = document.getElementById(`cell${i + 1}`);
+  if (cellElement !== null) {
+    const cell = new Cell('empty', cellElement);
+    cell.init();
+    return cell;
+  }
+});
+
+console.log(board);
 
 export {};
