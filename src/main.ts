344dddd9bc5185864f93739abfe4c273a7b089fd
KoutaKawase <suhrkawase@gmail.com>
2023-01-03 11:15:46 +0900

    feat: 🎸 wip

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import 'virtual:windi.css';
 import './sudoku.css';
 
 type CellState = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
-const boardElementCss =
-  'border-2 border-neutral-500 grid grid-cols-3 grid-rows-3 w-4/5 mt-10';
 
 class Cell {
   private state: CellState = 0;
@@ -29,21 +27,45 @@ class Cell {
   }
 }
 
-//ボードの状態を表す２次元配列を作成
-// let count = 0;
-// const board = [...Array(9)].map(() => {
-//   return [...Array(9)].map(() => {
-//     count += 1;
+class Board {
+  private cells: CellState[][];
 
-//     const cellElement = document.getElementById(`cell${count}`)!;
-//     const cell = new Cell(2, cellElement);
-//     cell.init();
+  constructor() {
+    //prettier-ignore
+    this.cells = [
+      [1, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+  }
 
-//     return cell;
-//   });
-// });
+  async render() {
+    let cellNumber = 0;
+    for (let col = 0; col < this.cells.length; col++) {
+      for (let row = 0; row < this.cells.length; row++) {
+        cellNumber += 1;
+        const cellElement = document.getElementById(`cell${cellNumber}`)!;
 
-function main() {}
+        if (this.cells[col][row] === 0) {
+          cellElement.style.color = 'white';
+        }
+
+        cellElement.textContent = `${this.cells[col][row]}`;
+      }
+    }
+  }
+}
+
+function main() {
+  const board = new Board();
+  board.render();
+}
 
 main();
 
