26580541f662aec1640b0df98c753b33c73a3715
KoutaKawase <suhrkawase@gmail.com>
2023-01-04 15:58:46 +0900

    refactor: 💡 wip

---
+++ src/cells.ts
@@ -0,0 +1,37 @@
+export type CellState = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+
+const BOARd_WIDTH = 9;
+const BOARD_HEIGHT = 9;
+const answer = [
+  [8, 1, 3, 7, 5, 2, 4, 6, 9],
+  [7, 5, 9, 1, 4, 6, 2, 3, 8],
+  [4, 6, 2, 3, 9, 8, 5, 7, 2],
+  [3, 9, 5, 2, 8, 1, 6, 4, 7],
+  [1, 7, 4, 5, 6, 3, 8, 9, 2],
+  [2, 8, 6, 4, 7, 9, 1, 5, 3],
+  [5, 4, 8, 9, 1, 7, 3, 2, 6],
+  [9, 2, 1, 6, 3, 5, 7, 8, 4],
+  [6, 3, 7, 8, 2, 4, 9, 1, 5],
+];
+
+export function init(cells: CellState[][]) {
+  let cellNumber = 0;
+
+  for (let col = 0; col < BOARD_HEIGHT; col++) {
+    for (let row = 0; row < BOARd_WIDTH; row++) {
+      cellNumber += 1;
+
+      const cellElement = document.getElementById(`cell${cellNumber}`)!;
+      render(cellElement, cells[col][row]);
+
+      cellElement.addEventListener('click', (e) => {
+        console.log((e.currentTarget as HTMLElement).id);
+      });
+    }
+  }
+}
+
+function render(elem: HTMLElement, cellState: CellState) {
+  elem.textContent = `${cellState}`;
+  if (cellState === 0) elem.style.color = 'white';
+}
--- src/main.ts
+++ src/main.ts
@@ -1,88 +1,22 @@
 import 'virtual:windi.css';
 import './sudoku.css';
 
-type CellState = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
-const example = [
-  [8, 1, 3, 7, 5, 2, 4, 6, 9],
-  [7, 5, 9, 1, 4, 6, 2, 3, 8],
-  [4, 6, 2, 3, 9, 8, 5, 7, 2],
-  [3, 9, 5, 2, 8, 1, 6, 4, 7],
-  [1, 7, 4, 5, 6, 3, 8, 9, 2],
-  [2, 8, 6, 4, 7, 9, 1, 5, 3],
-  [5, 4, 8, 9, 1, 7, 3, 2, 6],
-  [9, 2, 1, 6, 3, 5, 7, 8, 4],
-  [6, 3, 7, 8, 2, 4, 9, 1, 5],
+import { init, CellState } from './cells';
+
+const cells: CellState[][] = [
+  [0, 0, 3, 0, 0, 2, 0, 0, 0],
+  [0, 0, 0, 1, 0, 0, 0, 0, 0],
+  [4, 0, 2, 0, 0, 0, 0, 0, 1],
+  [0, 9, 0, 0, 8, 0, 6, 0, 7],
+  [1, 7, 0, 0, 0, 3, 8, 0, 2],
+  [0, 0, 0, 4, 0, 9, 0, 5, 0],
+  [5, 0, 8, 0, 0, 0, 3, 0, 0],
+  [0, 2, 0, 6, 3, 0, 0, 0, 4],
+  [0, 0, 0, 0, 0, 0, 9, 1, 0],
 ];
 
-class Cell {
-  private state: CellState = 0;
-  private readonly element: HTMLElement;
+const gameState = {
+  cells,
+};
 
-  constructor(state: CellState, elem: HTMLElement) {
-    this.state = state;
-    this.element = elem;
-  }
-
-  init() {
-    this.element.addEventListener('click', () => {
-      console.log(this.state);
-    });
-  }
-
-  draw() {
-    if (this.state === 0) {
-      this.element.textContent = '';
-      return;
-    }
-    this.element.textContent = `${this.state}`;
-  }
-}
-
-class Board {
-  private cells: Cell[][] = [[], [], [], [], [], [], [], [], []];
-
-  init() {
-    const problem: CellState[][] = [
-      [8, 1, 3, 7, 5, 2, 4, 6, 9],
-      [7, 5, 9, 1, 4, 6, 2, 3, 8],
-      [4, 6, 2, 3, 9, 8, 5, 7, 2],
-      [3, 9, 5, 2, 8, 1, 6, 4, 7],
-      [1, 7, 4, 5, 6, 3, 8, 9, 2],
-      [2, 8, 6, 4, 7, 9, 1, 5, 3],
-      [5, 4, 8, 9, 1, 7, 3, 2, 6],
-      [9, 2, 1, 6, 3, 5, 7, 8, 4],
-      [6, 3, 7, 8, 2, 4, 9, 1, 5],
-    ];
-    let cellNumber = 0;
-
-    for (let col = 0; col < problem.length; col++) {
-      for (let row = 0; row < problem.length; row++) {
-        cellNumber += 1;
-
-        const cellElement = document.getElementById(`cell${cellNumber}`)!;
-        const cell = new Cell(problem[col][row], cellElement);
-        cell.init();
-
-        this.cells[col][row] = cell;
-      }
-    }
-  }
-
-  async render() {
-    for (let col = 0; col < this.cells.length; col++) {
-      for (let row = 0; row < this.cells.length; row++) {
-        this.cells[col][row].draw();
-      }
-    }
-  }
-}
-
-function main() {
-  const board = new Board();
-  board.init();
-  board.render();
-}
-
-main();
-
-export {};
+init(gameState.cells);
